b134d26215ddfa6e04980ed8703440b37b0189d8
Rodmr092 <rodrigo.maciel.rubio@gmail.com>
2024-05-19 11:29:32 -0600

    Unused/abandoned projects deletion

--- Clone of Tactical Momentum Rank/universe.py
+++
@@ -1,8 +0,0 @@
- #region imports
-from AlgorithmImports import *
-#endregion
-
-class QQQConstituentsUniverseSelectionModel(ETFConstituentsUniverseSelectionModel):
-    def __init__(self, universe_settings: UniverseSettings = None) -> None:
-        symbol = Symbol.Create("QQQ", SecurityType.Equity, Market.USA)
-        super().__init__(symbol, universe_settings, lambda constituents: [c.Symbol for c in constituents]) 

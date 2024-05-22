5388690fb6eace87e28fc631bdcc678095f0328b
Rodmr092 <rodrigo.maciel.rubio@gmail.com>
2024-05-22 12:53:16 -0600

    passed a few functions to helper.py, cleanup of research notebook, begin strategy 2 (risk target, position scaling and capital implementation)

--- advanced futures trading strategies/helper.py
+++ advanced futures trading strategies/helper.py
@@ -15,33 +15,69 @@ expiration_dates = {
     'MES': [calculate_expiration_date(year, month) for year in range(2019, 2024) for month in [3, 6, 9, 12]]
 }
 
+def calculate_roll_date(expiration_date):
+    roll_date = expiration_date - timedelta(weeks=1)
+    return roll_date
+    
 def cumulated_returns(symbol, previous_price, current_price, instruments):
     params = instruments[symbol]
     delta_price = current_price - previous_price
     return_value = delta_price * params['multiplier']
     return return_value
 
+
+
 def calculate_return(symbol, price_series, instruments):
     params = instruments[symbol]
     multiplier = params['multiplier']
-    
     # Calculate daily returns in price points
     price_returns = price_series.diff().dropna()
-    
     # Calculate daily returns in currency
     currency_returns = price_returns * multiplier
-    
     # Create a DataFrame
     return_df = pd.DataFrame({
         'price_return': price_returns,
         'currency_return': currency_returns
     })
-    
     return return_df
 
-def calculate_roll_date(expiration_date):
-    roll_date = expiration_date - timedelta(weeks=1)
-    return roll_date
+def calculate_percentage_return(symbol, price_series, instruments, num_contracts):
+    # Extraer el multiplicador del diccionario de instrumentos
+    params = instruments.get(symbol, {})
+    multiplier = params.get('multiplier', 1)
+
+    # Calcular los cambios en la serie de precios
+    price_changes = price_series.diff().fillna(0)  # Usar fillna(0) para manejar el valor NaN de la primera observación
+
+    # Calcular retornos en dólares para cada paso de tiempo
+    returns_in_dollars = price_changes * multiplier * num_contracts
+
+    # Calcular los retornos porcentuales basados en el precio de la observación anterior
+    percentage_returns = (returns_in_dollars / (price_series.shift(1) * multiplier * num_contracts).fillna(1)) * 100
+
+    # Retornar la serie de retornos porcentuales
+    return percentage_returns
+
+
+def calculate_and_merge_returns(symbol, price_series, instruments):
+    """
+    Inputs 
+    symbol: str - a symbol to calculate returns for
+    price_series: pd.Series - a price series to calculate returns from
+    instruments: dict - a dictionary of instruments with metadata
+
+    Returns
+    merged_df: pd.DataFrame - a DataFrame with both return and percentage return columns
+    """
+    returns_df = calculate_return(symbol, price_series, instruments)
+    percentage_returns = calculate_percentage_return(symbol, price_series, instruments, num_contracts=1)
+    
+    # Unir los DataFrames en base al índice
+    merged_df = returns_df.join(percentage_returns.rename('percentage_return'))
+    # Clean up the memory
+    del returns_df
+    del percentage_returns
+    return merged_df
 
 
 def calculate_return_in_usd(symbol, initial_price, final_price, instruments, num_contracts=1):
@@ -75,22 +111,6 @@ def calculate_transaction_costs(params, history_df, start_date, end_date, num_co
             transaction_costs += ((spread * multiplier) + commission) * 2 * num_contracts
     return transaction_costs
 
-def calculate_percentage_return(symbol, price_series, instruments, num_contracts):
-    # Extraer el multiplicador del diccionario de instrumentos
-    params = instruments.get(symbol, {})
-    multiplier = params.get('multiplier', 1)
-
-    # Calcular los cambios en la serie de precios
-    price_changes = price_series.diff().fillna(0)  # Usar fillna(0) para manejar el valor NaN de la primera observación
-
-    # Calcular retornos en dólares para cada paso de tiempo
-    returns_in_dollars = price_changes * multiplier * num_contracts
-
-    # Calcular los retornos porcentuales basados en el precio de la observación anterior
-    percentage_returns = (returns_in_dollars / (price_series.shift(1) * multiplier * num_contracts).fillna(1)) * 100
-
-    # Retornar la serie de retornos porcentuales
-    return percentage_returns
 
 
 if __name__ == '__main__':
